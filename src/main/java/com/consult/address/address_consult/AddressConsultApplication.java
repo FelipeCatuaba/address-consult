56bc17df024cb32d8ca8d36a03b694cfec6d5f36
Felipe Bianchi Catuaba <felipe.catuaba@gmail.com>
2026-05-27 19:39:08 -0300

    initial commit

---
+++ src/main/java/com/consult/address/address_consult/AddressConsultApplication.java
@@ -0,0 +1,13 @@
+package com.consult.address.address_consult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressConsultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressConsultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/consult/address/address_consult/AddressConsultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consult.address.address_consult;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressConsultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
